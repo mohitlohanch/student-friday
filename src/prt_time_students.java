c02b9e0329ab2fe307918b073bd37b5253f1f9d3
Mohit Lohanch <lohanch@sheridancollege.ca>
2022-05-27 14:14:31 -0400

    added part time sttudent class

---
+++ src/prt_time_students.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author surender
+ */
+public class prt_time_students extends Student{
+    
+    private int noOfCourses;
+    
+    pubic prt_time_student (int noOfCourses, int studentId,)
+            super(studentID, studentName);
+            this.noOfCourses = noOfCourses;
+    
+}
+
+    public int getNoOfCourses() {
+           return noOfCourses;
+}
